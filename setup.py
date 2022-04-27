8f10c16963070d684089fe6fb78ee2983d9f71dd
Jeff Triplett <jeff.triplett@gmail.com>
2022-04-26 22:24:43 -0500

    :tractor: Make project easier to install

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name='django-pancake',
-    version='0.1',
-    description='Library for "flattening" Django templates.',
-    author='Adrian Holovaty',
-    author_email='adrian@holovaty.com',
-    url='https://github.com/adrianholovaty/django-pancake',
-    license='MIT',
-    classifiers = [
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-    ],
-    packages=['django_pancake'],
-)
+setup()
