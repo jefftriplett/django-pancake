6f25914ece47b2f1810ceb864c0c66baab320a74
Jeff Triplett <jeff.triplett@gmail.com>
2022-04-26 22:24:57 -0500

    :tractor: Port to modern Django

--- django_pancake/flatten.py
+++ django_pancake/flatten.py
@@ -1,24 +1,26 @@
-from django.template.base import Lexer, TOKEN_BLOCK, TOKEN_TEXT, TOKEN_VAR
 import os
-import re
+
+from django.template.base import Lexer, TokenType
+
 
 class PancakeFail(Exception):
     pass
 
-class ASTNode(object):
+
+class ASTNode:
     "A node in the AST."
+
     def __init__(self, name):
         self.name = name
-        self.leaves = [] # Each leaf can be a string or another ASTNode.
+        self.leaves = []  # Each leaf can be a string or another ASTNode.
 
     def __repr__(self):
-        return '<%s %s>' % (self.__class__.__name__, self.name)
+        return f"<{self.__class__.__name__} {self.name}>"
 
     def sub_text(self):
         for leaf in self.leaves:
             if isinstance(leaf, ASTNode):
-                for subleaf in leaf.sub_text():
-                    yield subleaf
+                yield from leaf.sub_text()
             else:
                 yield leaf
 
@@ -26,23 +28,29 @@ class ASTNode(object):
         for leaf in self.leaves:
             if isinstance(leaf, ASTNode):
                 yield leaf
-                for subleaf in leaf.sub_nodes():
-                    yield subleaf
+                yield from leaf.sub_nodes()
+
 
 class Template(ASTNode):
     "Root node of the AST. Represents a template, which may or may not have a parent."
+
     def __init__(self, name):
-        super(Template, self).__init__(name)
-        self.parent = None # Template object for the parent template, if there's a parent.
-        self.blocks = {} # Maps block names to objects in self.leaves for quick lookup.
-        self.loads = set() # Template libraries to load.
+        super().__init__(name)
+        self.parent = (
+            None  # Template object for the parent template, if there's a parent.
+        )
+        self.blocks = {}  # Maps block names to objects in self.leaves for quick lookup.
+        self.loads = set()  # Template libraries to load.
+
 
 class Block(ASTNode):
     "Represents a {% block %}."
     pass
 
-class TemplateDirectory(object):
+
+class TemplateDirectory:
     "Dictionary-like object that maps template names to template strings."
+
     def __init__(self, directory):
         self.directory = directory
 
@@ -50,7 +58,8 @@ class TemplateDirectory(object):
         filename = os.path.join(self.directory, template_name)
         return open(filename).read()
 
-class Parser(object):
+
+class Parser:
     def __init__(self, fail_gracefully=True):
         self.fail_gracefully = fail_gracefully
 
@@ -58,12 +67,16 @@ class Parser(object):
         """
         Creates an AST for the given template. Returns a Template object.
         """
-        self.templates = templates # Maps template names to template sources.
+        self.templates = templates  # Maps template names to template sources.
         self.root = Template(template_name)
         self.stack = [self.root]
         self.current = self.root
-        self.tokens = Lexer(self.templates[template_name], 'django-pancake').tokenize()
-        _TOKEN_TEXT, _TOKEN_VAR, _TOKEN_BLOCK = TOKEN_TEXT, TOKEN_VAR, TOKEN_BLOCK
+        self.tokens = Lexer(self.templates[template_name]).tokenize()
+        _TOKEN_TEXT, _TOKEN_VAR, _TOKEN_BLOCK = (
+            TokenType.TEXT,
+            TokenType.VAR,
+            TokenType.BLOCK,
+        )
         while self.tokens:
             token = self.next_token()
 
@@ -71,30 +84,34 @@ class Parser(object):
                 self.current.leaves.append(token.contents)
 
             elif token.token_type == _TOKEN_VAR:
-                if token.contents == 'block.super':
+                if token.contents == "block.super":
                     if self.root.parent is None:
-                        raise PancakeFail('Got {{ block.super }} in a template that has no parent')
+                        raise PancakeFail(
+                            "Got {{ block.super }} in a template that has no parent"
+                        )
 
                     super_block_name = self.stack[-1].name
                     current_par = self.root.parent
                     while current_par is not None:
                         if super_block_name in current_par.blocks:
-                            self.current.leaves.extend(current_par.blocks[super_block_name].leaves)
+                            self.current.leaves.extend(
+                                current_par.blocks[super_block_name].leaves
+                            )
                             break
                         current_par = current_par.parent
                 else:
-                    self.current.leaves.append('{{ %s }}' % token.contents)
+                    self.current.leaves.append("{{ %s }}" % token.contents)
 
             elif token.token_type == _TOKEN_BLOCK:
                 try:
                     tag_name, arg = token.contents.split(None, 1)
                 except ValueError:
                     tag_name, arg = token.contents.strip(), None
-                method_name = 'do_%s' % tag_name
+                method_name = "do_%s" % tag_name
                 if hasattr(self, method_name):
                     getattr(self, method_name)(arg)
                 else:
-                    self.current.leaves.append('{%% %s %%}' % token.contents)
+                    self.current.leaves.append("{%% %s %%}" % token.contents)
 
         return self.root
 
@@ -103,7 +120,7 @@ class Parser(object):
 
     def do_block(self, text):
         if not text:
-            raise PancakeFail('{% block %} without a name')
+            raise PancakeFail("{% block %} without a name")
         self.current.leaves.append(Block(text))
         self.root.blocks[text] = self.current = self.current.leaves[-1]
         self.stack.append(self.current)
@@ -114,23 +131,28 @@ class Parser(object):
 
     def do_extends(self, text):
         if not text:
-            raise PancakeFail('{%% extends %%} without an argument (file: %r)' % self.root.name)
+            raise PancakeFail(
+                "{%% extends %%} without an argument (file: %r)" % self.root.name
+            )
         if text[0] in ('"', "'"):
             parent_name = text[1:-1]
             self.root.parent = Parser().parse(parent_name, self.templates)
         else:
-            raise PancakeFail('Variable {%% extends %%} tags are not supported (file: %r)' % self.root.name)
+            raise PancakeFail(
+                "Variable {%% extends %%} tags are not supported (file: %r)"
+                % self.root.name
+            )
 
     def do_comment(self, text):
         # Consume all tokens until 'endcomment'
         while self.tokens:
             token = self.next_token()
-            if token.token_type == TOKEN_BLOCK:
+            if token.token_type == TokenType.BLOCK:
                 try:
                     tag_name, arg = token.contents.split(None, 1)
                 except ValueError:
                     tag_name, arg = token.contents.strip(), None
-                if tag_name == 'endcomment':
+                if tag_name == "endcomment":
                     break
 
     def do_load(self, text):
@@ -139,24 +161,30 @@ class Parser(object):
         self.root.loads.update(text.split())
 
     def do_include(self, text):
-        if ' only' in text:
+        if " only" in text:
             if self.fail_gracefully:
-                self.current.leaves.append('{%% include %s %%}' % text)
+                self.current.leaves.append("{%% include %s %%}" % text)
                 return
             else:
-                raise PancakeFail('{%% include %%} tags containing "only" are not supported (file: %r)' % self.root.name)
+                raise PancakeFail(
+                    '{%% include %%} tags containing "only" are not supported (file: %r)'
+                    % self.root.name
+                )
         try:
             template_name, rest = text.split(None, 1)
         except ValueError:
-            template_name, rest = text, ''
+            template_name, rest = text, ""
         if not template_name[0] in ('"', "'"):
             if self.fail_gracefully:
-                self.current.leaves.append('{%% include %s %%}' % text)
+                self.current.leaves.append("{%% include %s %%}" % text)
                 return
             else:
-                raise PancakeFail('Variable {%% include %%} tags are not supported (file: %r)' % self.root.name)
+                raise PancakeFail(
+                    "Variable {%% include %%} tags are not supported (file: %r)"
+                    % self.root.name
+                )
         template_name = template_name[1:-1]
-        if rest.startswith('with '):
+        if rest.startswith("with "):
             rest = rest[5:]
 
         include_node = Parser().parse(template_name, self.templates)
@@ -165,10 +193,11 @@ class Parser(object):
         self.root.loads.update(include_node.loads)
 
         if rest:
-            self.current.leaves.append('{%% with %s %%}' % rest)
+            self.current.leaves.append("{%% with %s %%}" % rest)
         self.current.leaves.extend(include_node.leaves)
         if rest:
-            self.current.leaves.append('{% endwith %}')
+            self.current.leaves.append("{% endwith %}")
+
 
 def flatten_ast(template):
     "Given an AST as returned by the parser, returns a string of flattened template text."
@@ -200,16 +229,19 @@ def flatten_ast(template):
     # Put them in alphabetical order to be consistent.
     if master.loads:
         loads = sorted(master.loads)
-        master.leaves.insert(0, '{%% load %s %%}' % ' '.join(loads))
+        master.leaves.insert(0, "{%% load %s %%}" % " ".join(loads))
 
     return master
+
 
 def flatten(template_name, templates):
     p = Parser()
     template = p.parse(template_name, templates)
     flat = flatten_ast(template)
-    return ''.join(flat.sub_text())
+    return "".join(flat.sub_text())
+
 
 if __name__ == "__main__":
     import sys
-    print flatten(sys.argv[1], TemplateDirectory(sys.argv[2]))
+
+    print(flatten(sys.argv[1], TemplateDirectory(sys.argv[2])))
--- tests/test_pancake.py
+++ tests/test_pancake.py
@@ -3,94 +3,175 @@ from django_pancake.flatten import flatten
 # template_name: (template_source, pancake_source),
 TESTS = {
     # No inheritance.
-    'test1': ('Some text here', 'Some text here'),
-    'test2': ('{% unknown_block_tag %}Text here', '{% unknown_block_tag %}Text here'),
-
+    "test1": ("Some text here", "Some text here"),
+    "test2": ("{% unknown_block_tag %}Text here", "{% unknown_block_tag %}Text here"),
     # Basic inheritance.
-    'base1': ('<html><title>{% block title %}{% endblock %}</title></html>', '<html><title></title></html>'),
-    'child1': ('{% extends "base1" %} {% block title %}It worked{% endblock %}', '<html><title>It worked</title></html>'),
-
+    "base1": (
+        "<html><title>{% block title %}{% endblock %}</title></html>",
+        "<html><title></title></html>",
+    ),
+    "child1": (
+        '{% extends "base1" %} {% block title %}It worked{% endblock %}',
+        "<html><title>It worked</title></html>",
+    ),
     # Empty child.
-    'emptychild1': ('{% extends "base1" %}', '<html><title></title></html>'),
-
+    "emptychild1": ('{% extends "base1" %}', "<html><title></title></html>"),
     # Junk in child templates.
-    'junk1': ('{% extends "base1" %} Junk goes here', '<html><title></title></html>'),
-    'junk2': ('{% extends "base1" %} Junk goes here {% some_tag %}', '<html><title></title></html>'),
-    'junk3': ('{% extends "base1" %} Junk goes here {% some_tag %}  {% block title %}Worked{% endblock %}', '<html><title>Worked</title></html>'),
-
+    "junk1": ('{% extends "base1" %} Junk goes here', "<html><title></title></html>"),
+    "junk2": (
+        '{% extends "base1" %} Junk goes here {% some_tag %}',
+        "<html><title></title></html>",
+    ),
+    "junk3": (
+        '{% extends "base1" %} Junk goes here {% some_tag %}  {% block title %}Worked{% endblock %}',
+        "<html><title>Worked</title></html>",
+    ),
     # Inheritance with default values in blocks.
-    'defaultbase1': ('<title>{% block title %}Default{% endblock %}</title>', '<title>Default</title>'),
-    'default1': ('{% extends "defaultbase1" %}', '<title>Default</title>'),
-    'default2': ('{% extends "defaultbase1" %}{% block title %}No default{% endblock %}', '<title>No default</title>'),
-
+    "defaultbase1": (
+        "<title>{% block title %}Default{% endblock %}</title>",
+        "<title>Default</title>",
+    ),
+    "default1": ('{% extends "defaultbase1" %}', "<title>Default</title>"),
+    "default2": (
+        '{% extends "defaultbase1" %}{% block title %}No default{% endblock %}',
+        "<title>No default</title>",
+    ),
     # Blocks within blocks.
-    'withinbase1': ('<title>{% block fulltitle %}{% block title %}Welcome{% endblock %} | Example.com{% endblock %}</title>', '<title>Welcome | Example.com</title>'),
-    'within1': ('{% extends "withinbase1" %}  {% block fulltitle %}Yay{% endblock %}', '<title>Yay</title>'),
-    'within2': ('{% extends "withinbase1" %}  {% block title %}Some page{% endblock %}', '<title>Some page | Example.com</title>'),
-
+    "withinbase1": (
+        "<title>{% block fulltitle %}{% block title %}Welcome{% endblock %} | Example.com{% endblock %}</title>",
+        "<title>Welcome | Example.com</title>",
+    ),
+    "within1": (
+        '{% extends "withinbase1" %}  {% block fulltitle %}Yay{% endblock %}',
+        "<title>Yay</title>",
+    ),
+    "within2": (
+        '{% extends "withinbase1" %}  {% block title %}Some page{% endblock %}',
+        "<title>Some page | Example.com</title>",
+    ),
     # Blocks within blocks, overriding both blocks.
-    'bothblocks1': ('{% extends "withinbase1" %}{% block fulltitle %}Outer{% endblock %}{% block title %}Inner{% endblock %}', '<title>Outer</title>'),
-    'bothblocks2': ('{% extends "withinbase1" %}{% block title %}Inner{% endblock %}{% block fulltitle %}Outer{% endblock %}', '<title>Outer</title>'),
-
+    "bothblocks1": (
+        '{% extends "withinbase1" %}{% block fulltitle %}Outer{% endblock %}{% block title %}Inner{% endblock %}',
+        "<title>Outer</title>",
+    ),
+    "bothblocks2": (
+        '{% extends "withinbase1" %}{% block title %}Inner{% endblock %}{% block fulltitle %}Outer{% endblock %}',
+        "<title>Outer</title>",
+    ),
     # Three-level inheritance structure.
-    '3levelbase1': ('{% block content %}Welcome!<br>{% block header %}{% endblock %}{% endblock %}', 'Welcome!<br>'),
-    '3levelbase2': ('{% extends "3levelbase1" %}{% block header %}<h1>{% block h1 %}{% endblock %}</h1>{% endblock %}', 'Welcome!<br><h1></h1>'),
-    '3level1': ('{% extends "3levelbase2" %}{% block h1 %}Title goes here{% endblock %}', 'Welcome!<br><h1>Title goes here</h1>'),
-
+    "3levelbase1": (
+        "{% block content %}Welcome!<br>{% block header %}{% endblock %}{% endblock %}",
+        "Welcome!<br>",
+    ),
+    "3levelbase2": (
+        '{% extends "3levelbase1" %}{% block header %}<h1>{% block h1 %}{% endblock %}</h1>{% endblock %}',
+        "Welcome!<br><h1></h1>",
+    ),
+    "3level1": (
+        '{% extends "3levelbase2" %}{% block h1 %}Title goes here{% endblock %}',
+        "Welcome!<br><h1>Title goes here</h1>",
+    ),
     # Wacky bug: Four-level inheritance structure.
-    'wackylevel1': ('{% block content %}{% endblock %}', ''),
-    'wackylevel2': ('{% extends "wackylevel1" %} {% block content %}<div id="canvas">{% block canvas %}{% endblock %}</div><div id="rail">{% block rail %}{% endblock %}</div>{% endblock %}',
-                    '<div id="canvas"></div><div id="rail"></div>'),
-    'wackylevel3': ('{% extends "wackylevel2" %} {% block content %}<div id="rail">{% block rail %}{% endblock %}</div><div id="canvas">{% block canvas %}{% endblock %}</div>{% endblock %}',
-                    '<div id="rail"></div><div id="canvas"></div>'),
-    'wackylevel4': ('{% extends "wackylevel3" %}{% block rail %}Rail{% endblock %}{% block canvas %}Canvas{% endblock %}',
-                    '<div id="rail">Rail</div><div id="canvas">Canvas</div>'),
-
+    "wackylevel1": ("{% block content %}{% endblock %}", ""),
+    "wackylevel2": (
+        '{% extends "wackylevel1" %} {% block content %}<div id="canvas">{% block canvas %}{% endblock %}</div><div id="rail">{% block rail %}{% endblock %}</div>{% endblock %}',
+        '<div id="canvas"></div><div id="rail"></div>',
+    ),
+    "wackylevel3": (
+        '{% extends "wackylevel2" %} {% block content %}<div id="rail">{% block rail %}{% endblock %}</div><div id="canvas">{% block canvas %}{% endblock %}</div>{% endblock %}',
+        '<div id="rail"></div><div id="canvas"></div>',
+    ),
+    "wackylevel4": (
+        '{% extends "wackylevel3" %}{% block rail %}Rail{% endblock %}{% block canvas %}Canvas{% endblock %}',
+        '<div id="rail">Rail</div><div id="canvas">Canvas</div>',
+    ),
     # Inheritance, skipping a level.
-    'skiplevel1': ('{% block header %}{% block h1 %}{% endblock %}<p>Header</p>{% endblock %}', '<p>Header</p>'),
-    'skiplevel2': ('{% extends "skiplevel1" %}', '<p>Header</p>'),
-    'skiplevel3': ('{% extends "skiplevel2" %}{% block h1 %}<h1>Title</h1>{% endblock %}', '<h1>Title</h1><p>Header</p>'),
-
+    "skiplevel1": (
+        "{% block header %}{% block h1 %}{% endblock %}<p>Header</p>{% endblock %}",
+        "<p>Header</p>",
+    ),
+    "skiplevel2": ('{% extends "skiplevel1" %}', "<p>Header</p>"),
+    "skiplevel3": (
+        '{% extends "skiplevel2" %}{% block h1 %}<h1>Title</h1>{% endblock %}',
+        "<h1>Title</h1><p>Header</p>",
+    ),
     # {% load %} statements get bubbled up for inheritance.
-    'loadbase1': ('{% load webdesign %}<html><title>{% block title %}{% endblock %}</title></html>', '{% load webdesign %}<html><title></title></html>'),
-    'load1': ('{% extends "base1" %}{% load humanize %}{% block title %}Load 1{% endblock %}', '{% load humanize %}<html><title>Load 1</title></html>'),
-    'load2': ('{% extends "loadbase1" %}{% load humanize %}{% block title %}Load 2{% endblock %}', '{% load humanize webdesign %}<html><title>Load 2</title></html>'),
-
+    "loadbase1": (
+        "{% load webdesign %}<html><title>{% block title %}{% endblock %}</title></html>",
+        "{% load webdesign %}<html><title></title></html>",
+    ),
+    "load1": (
+        '{% extends "base1" %}{% load humanize %}{% block title %}Load 1{% endblock %}',
+        "{% load humanize %}<html><title>Load 1</title></html>",
+    ),
+    "load2": (
+        '{% extends "loadbase1" %}{% load humanize %}{% block title %}Load 2{% endblock %}',
+        "{% load humanize webdesign %}<html><title>Load 2</title></html>",
+    ),
     # {% load %} statements get bubbled up for includes.
-    'loadinclude1': ('{% load webdesign %}Hello', '{% load webdesign %}Hello'),
-    'load3': ('{% load foo %}{% include "loadinclude1" %} there', '{% load foo webdesign %}Hello there'),
-
+    "loadinclude1": ("{% load webdesign %}Hello", "{% load webdesign %}Hello"),
+    "load3": (
+        '{% load foo %}{% include "loadinclude1" %} there',
+        "{% load foo webdesign %}Hello there",
+    ),
     # block.super.
-    'super1': ('{% extends "withinbase1" %}{% block title %}{{ block.super }} to the site{% endblock %}', '<title>Welcome to the site | Example.com</title>'),
-    'super2': ('{% extends "withinbase1" %}{% block title %}{{ block.super }} {{ block.super }} {{ block.super }}{% endblock %}', '<title>Welcome Welcome Welcome | Example.com</title>'),
-
+    "super1": (
+        '{% extends "withinbase1" %}{% block title %}{{ block.super }} to the site{% endblock %}',
+        "<title>Welcome to the site | Example.com</title>",
+    ),
+    "super2": (
+        '{% extends "withinbase1" %}{% block title %}{{ block.super }} {{ block.super }} {{ block.super }}{% endblock %}',
+        "<title>Welcome Welcome Welcome | Example.com</title>",
+    ),
     # block.super, skipping a level.
-    'superskip1': ('{% block header %}{% block h1 %}{% endblock %}<p>Header</p>{% endblock %}', '<p>Header</p>'),
-    'superskip2': ('{% extends "superskip1" %}', '<p>Header</p>'),
-    'superskip3': ('{% extends "superskip2" %}{% block header %}Here: {{ block.super }}{% endblock %}', 'Here: <p>Header</p>'),
-
+    "superskip1": (
+        "{% block header %}{% block h1 %}{% endblock %}<p>Header</p>{% endblock %}",
+        "<p>Header</p>",
+    ),
+    "superskip2": ('{% extends "superskip1" %}', "<p>Header</p>"),
+    "superskip3": (
+        '{% extends "superskip2" %}{% block header %}Here: {{ block.super }}{% endblock %}',
+        "Here: <p>Header</p>",
+    ),
     # Include tag.
-    'include1': ('<head>{% include "defaultbase1" %}</head>', '<head><title>Default</title></head>'),
-    'include2': ('<head>{% include "defaultbase1" with foo=bar %}</head>', '<head>{% with foo=bar %}<title>Default</title>{% endwith %}</head>'),
-    'include3': ('<head>{% include "defaultbase1" with foo=bar baz=3 %}</head>', '<head>{% with foo=bar baz=3 %}<title>Default</title>{% endwith %}</head>'),
-
+    "include1": (
+        '<head>{% include "defaultbase1" %}</head>',
+        "<head><title>Default</title></head>",
+    ),
+    "include2": (
+        '<head>{% include "defaultbase1" with foo=bar %}</head>',
+        "<head>{% with foo=bar %}<title>Default</title>{% endwith %}</head>",
+    ),
+    "include3": (
+        '<head>{% include "defaultbase1" with foo=bar baz=3 %}</head>',
+        "<head>{% with foo=bar baz=3 %}<title>Default</title>{% endwith %}</head>",
+    ),
     # Include tag with 'only'.
-    'includeonly1': ('<head>{% include "defaultbase1" only %}</head>', '<head>{% include "defaultbase1" only %}</head>'),
-
+    "includeonly1": (
+        '<head>{% include "defaultbase1" only %}</head>',
+        '<head>{% include "defaultbase1" only %}</head>',
+    ),
     # Include tag with variable argument.
-    'includevariable1': ('<head>{% include some_template %}</head>', '<head>{% include some_template %}</head>'),
-
+    "includevariable1": (
+        "<head>{% include some_template %}</head>",
+        "<head>{% include some_template %}</head>",
+    ),
     # Remove template comments.
-    'comments1': ('lo{% comment %}Long-style comment{% endcomment %}ve', 'love'),
-    'comments2': ('lo{# Short-style comment #}ve', 'love'),
-    'comments3': ('lo{% comment %}{% if foo %}foo{% else %}bar{% endif %}{# Inner comment #}Some other stuff{% endcomment %}ve', 'love'),
+    "comments1": ("lo{% comment %}Long-style comment{% endcomment %}ve", "love"),
+    "comments2": ("lo{# Short-style comment #}ve", "love"),
+    "comments3": (
+        "lo{% comment %}{% if foo %}foo{% else %}bar{% endif %}{# Inner comment #}Some other stuff{% endcomment %}ve",
+        "love",
+    ),
 }
-TEMPLATES = dict((k, v[0]) for k, v in TESTS.items())
+TEMPLATES = {k: v[0] for k, v in TESTS.items()}
+
 
 def test_flatten():
     for template_name, (template_source, pancake_source) in TESTS.items():
         yield check_flatten, template_name, pancake_source
 
+
 def check_flatten(template_name, expected):
     result = flatten(template_name, TEMPLATES)
-    assert result == expected, 'expected %r, got %r' % (expected, result)
+    assert result == expected, f"expected {expected!r}, got {result!r}"
